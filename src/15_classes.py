ccde4c35b223a220fbd8129299b7f74f0a6c3ae6
Christine Fletcher <chrismis79@gmail.com>
2020-07-07 18:34:34 -0500

    finished classes exercises not completed

--- src/15_classes.py
+++ src/15_classes.py
@@ -28,6 +28,9 @@ class Waypoints(LatLon):
     def __init__(self, name, lt, lg):
         self.name = name
         super().__init__(lt, lg)
+    
+    def __str__(self):
+        return {'name':self.name, 'lt':self.lat, 'lg':self.lon}
 
     def display_waypoint(self):
         print('Name is = ' + str(self.name))
@@ -35,25 +38,43 @@ class Waypoints(LatLon):
         print('Longitude is = ' + str(self.lon))
        
 
-obj = Waypoints("Waypoint", 42, 100)
+objw = Waypoints("Waypoint", 42, 100)
     
-obj.display_waypoint()
+objw.display_waypoint()
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoints):
+    def __init__(self, name, difficulty, size, lt, lg):
+        self.difficulty = difficulty
+        self.size = size
+        super().__init__(name, lt, lg)
+    
+    def display_geo(self):
+        print('This is difficulty ' + str(self.difficulty))
+        print('This is size ' + str(self.size))
+        print('This is lat from geo ' + str(self.lat))
+        print('This is lon from geo ' + str(self.lon))
+    
+objg = Geocache("Geo", "Hard", "Large", 28, 144)
+
+objg.display_geo()
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+waypoint = Waypoints("Catacombs", 41.70505, -121.51521)
+
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-#print(waypoint)
+print(waypoint.__str__())
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-# YOUR CODE HERE
+# YOUR CODE 
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
-#print(geocache)
+print(geocache.__str__())
