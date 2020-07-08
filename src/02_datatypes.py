b16a3480abaef43c1efeecbe42ad2cbd28ce81a8
Christine Fletcher <chrismis79@gmail.com>
2020-07-07 23:45:01 -0500

    scopes completed

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -12,10 +12,10 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(int(y) + x)
+
 
 # YOUR CODE HERE
-
+print(int(y) + x)
 
 # Write a print statement that combines x + y into the string value 57
 
