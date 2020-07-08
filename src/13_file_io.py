a71beecab6013f94a286a681b557563d91073a58
Christine Fletcher <chrismis79@gmail.com>
2020-07-07 23:33:25 -0500

    finished file_io and comprehensions

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+with open("src/foo.txt", "r") as f:
+    read_data = f.read()
+print(read_data)
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("src/bar.txt", 'a') as txt:
+    txt.write('This is line 1\n')
+    txt.write('This is line 2\n')
+    txt.write('This is line 3, creative huh?')
